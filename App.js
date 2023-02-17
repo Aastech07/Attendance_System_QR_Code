a7a435b66b8604a137ed049172efb407d5d7dc1c
Ansari AAS Mohammad <ansariaasmohammad17@gmail.com>
2023-02-17 03:05:31 -0800

    Initial commit
    Generated by create-expo-app 1.3.2.

---
+++ App.js
@@ -0,0 +1,47 @@
+import Login from './Component/Login';
+import HomeScreen from './Component/HomeScreen';
+import Account from './Component/Account';
+import Fetch from './Component/Fetch';
+import Component from './Component/Component';
+import FirstPage from './Component/FirstPage';
+import Admin from './Component/Admin';
+import Account1 from './Component/Account1';
+import LogOut from './Component/LogOut';
+import QR from './Component/QR';
+import Component1 from './Component/Component1';
+import {FetchData }from './Component/Fetch1';
+import QrCode from './Component/QrCode';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+ 
+const stack = createNativeStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer> 
+    <stack.Navigator>
+ <stack.Screen name ='Page' options={{ headerShown: false }}  component={FirstPage}/>
+  <stack.Screen name='Login' options={{headerShown:false }} component={Login}/>
+<stack.Screen name='Account' options={{ headerShown: false  }}  component={Account }/>
+  <stack.Screen name='Home' options={{ headerShown: false }}  component={HomeScreen}/>
+  <stack.Screen name='Comp' options={{ headerShown: false }}  component={Component}/>
+  <stack.Screen name='QrCode' options={{ headerShown: false }} component={QrCode}/>
+  <stack.Screen name='Fetch' options={{ headerShown: false }}  component={Fetch}/>
+ <stack.Screen name='Admin' options={{ headerShown: false }}  component={Admin}/>
+  <stack.Screen name='Account1' options={{ headerShown: false }}  component={Account1}/>
+  <stack.Screen name='LogOut' options={{ headerShown: false }}  component={LogOut}/>
+<stack.Screen name='QR' options={{ headerShown: false }}  component={QR}/>
+  <stack.Screen name='Component1' options={{ headerShown: false }}  component={Component1}/>
+<stack.Screen name='FetchData' options={{ headerShown: false }}  component={FetchData}/>
+
+
+
+</stack.Navigator>
+</NavigationContainer>
+
+
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+export const firebaseConfig={
+    apiKey: "AIzaSyCicQs4QERDI4GiffGxoPMV2nV6fD_BUnI",
+    authDomain: "mynewproject-286f9.firebaseapp.com",
+    databaseURL: "https://mynewproject-286f9-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mynewproject-286f9",
+    storageBucket: "mynewproject-286f9.appspot.com",
+    messagingSenderId: "1013283481664",
+    appId: "1:1013283481664:web:98492c6944534931ff97a5",
+    measurementId: "G-SKD30GBFG3"
+}
+if(!firebase.apps.length){
+   firebase.initializeApp(firebaseConfig) 
+}
+const auth = firebase.auth()
+export {auth,firebase}
